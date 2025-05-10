f770039a908aade73184de86eac681168df526c6
0Tick <96893104+0Tick@users.noreply.github.com>
2025-05-11 00:16:38 +0200

    Lucide React bump

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 
-import { useState, useCallback } from "react"
+import { useState, useCallback, JSX } from "react"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
